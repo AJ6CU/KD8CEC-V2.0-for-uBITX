172d189ade738928276f45686d8d5ccd77eacf19
Mark Hatch <58341254+mark2382@users.noreply.github.com>
2024-01-25 08:36:39 -0800

    put ux under github

---
+++ ubitx_20/squareline_ux_source/ubitx_20_ux_events.py
@@ -0,0 +1,72 @@
+
+def toggleVFO(event_struct):
+    return
+
+
+def bandDown(event_struct):
+    return
+
+
+def bandUpClicked(event_struct):
+    return
+
+
+def bandUp(event_struct):
+    return
+
+
+def bandDownClicked(event_struct):
+    return
+
+
+def toggleVFOClicked(event_struct):
+    return
+
+
+def setModeLSB(event_struct):
+    return
+
+
+def setModeUSB(event_struct):
+    return
+
+
+def setModeCWL(event_struct):
+    return
+
+
+def setModeCWU(event_struct):
+    return
+
+
+def toggleStopButton(event_struct):
+    return
+
+
+def toggleLockButton(event_struct):
+    return
+
+
+def toggleSDRButton(event_struct):
+    return
+
+
+def splitButtonClicked(event_struct):
+    return
+
+
+def ritButtonClicked(event_struct):
+    return
+
+
+def ifsButtonClicked(event_struct):
+    return
+
+
+def attButtonClicked(event_struct):
+    return
+
+
+def tuneRateSelectionClicked(event_struct):
+    return
+
